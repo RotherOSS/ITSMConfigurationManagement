0399a6976046f29811455edd2af359c9677d98fc
bernhard <Bernhard.Schmalhofer@gmx.de>
2023-07-27 14:42:57 +0200

    Remove TERMS AND CONDITIONS
    at the end of the files. The license information is alread stated
    on the top of the files.

--- Kernel/System/ITSMConfigItem.pm
+++ Kernel/System/ITSMConfigItem.pm
@@ -1945,8 +1945,6 @@ sub _PrepareLikeString {
     return;
 }
 
-1;
-
 =head1 ITSM Config Item events:
 
 ConfigItemCreate, VersionCreate, DeploymentStateUpdate, IncidentStateUpdate,
@@ -1955,12 +1953,4 @@ DefinitionCreate, VersionDelete
 
 =cut
 
-=head1 TERMS AND CONDITIONS
-
-This software is part of the OTOBO project (L<https://otobo.org/>).
-
-This software comes with ABSOLUTELY NO WARRANTY. For details, see
-the enclosed file COPYING for license information (GPL). If you
-did not receive this file, see L<https://www.gnu.org/licenses/gpl-3.0.txt>.
-
-=cut
+1;
