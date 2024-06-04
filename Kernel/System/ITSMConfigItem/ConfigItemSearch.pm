ca8cb2e5accb0f2d80eeac8b2975fd9273312ce8
Stefan Härter <sha@rother-oss.com>
2024-06-04 09:08:36 +0200

    Added additional search params for ConfigItemSearch.

--- Kernel/System/ITSMConfigItem/ConfigItemSearch.pm
+++ Kernel/System/ITSMConfigItem/ConfigItemSearch.pm
@@ -1,7 +1,7 @@
 # --
 # OTOBO is a web-based config iteming system for service organisations.
 # --
-# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -501,8 +501,10 @@ sub ConfigItemSearch {
 
     # other config item stuff
     my %FieldSQLMap = (
-        Number => 'ci.configitem_number',
-        Name   => 'v.name',
+        Number   => 'ci.configitem_number',
+        Name     => 'v.name',
+        CreateBy => 'ci.create_by',
+        ChangeBy => 'ci.change_by',
     );
 
     ATTRIBUTE:
