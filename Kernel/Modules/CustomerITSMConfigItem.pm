741da05a1d2fd1433bfb12ead2cec31200f84d10
Stefan Härter <sha@rother-oss.com>
2024-07-16 13:28:47 +0200

    Added column filter to search statements in CustomerITSMConfigItem overview.

--- Kernel/Modules/CustomerITSMConfigItem.pm
+++ Kernel/Modules/CustomerITSMConfigItem.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -328,6 +328,7 @@ sub Run {
             if ( @ViewableClassIDs && @ViewableDeplStateIDs ) {
                 $Count = $ConfigItemObject->ConfigItemSearch(
                     %FilterSearch,
+                    %ColumnFilter,
                     Result => 'COUNT',
                 );
             }
@@ -473,6 +474,7 @@ sub Run {
         {
             @OriginalViewableConfigItems = $ConfigItemObject->ConfigItemSearch(
                 %{ $Filters{$Filter}->{Search} },
+                %ColumnFilter,
                 Limit  => $Limit,
                 Result => 'ARRAY',
             );
