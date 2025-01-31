56e08b05755492a1da96eaca0e01b56076442406
Stefan Härter <sha@rother-oss.com>
2025-01-31 08:12:44 +0100

    Issue #37: Changed ConfigItemID param for name check in ConfigItemUpdate.

--- Kernel/System/ITSMConfigItem.pm
+++ Kernel/System/ITSMConfigItem.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
+# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -1020,7 +1020,7 @@ sub ConfigItemUpdate {
         if ( $Kernel::OM->Get('Kernel::Config')->Get('UniqueCIName::EnableUniquenessCheck') ) {
 
             my $NameDuplicates = $Self->UniqueNameCheck(
-                ConfigItemID => 'NEW',
+                ConfigItemID => $Param{ConfigItemID},
                 ClassID      => $Param{ClassID},
                 Name         => $Param{Name},
             );
