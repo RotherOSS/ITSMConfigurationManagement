41cdaeff2c6dcc1f1a1320ed7131b52427273067
bernhard <Bernhard.Schmalhofer@gmx.de>
2025-04-24 15:45:43 +0200

    Accept suggestions from CodePolicy
    First fix some POD, then accept formatting changes.

--- Kernel/System/ITSMConfigItem/Event/DoLinkCount.pm
+++ Kernel/System/ITSMConfigItem/Event/DoLinkCount.pm
@@ -36,12 +36,7 @@ All event handler functions for count licenses.
 
 =head1 PUBLIC INTERFACE
 
-=over 4
-
-=cut
-
-=item new()
-
+=head2 new()
 
     use Kernel::System::ObjectManager;
     local $Kernel::OM = Kernel::System::ObjectManager->new();
@@ -59,7 +54,7 @@ sub new {
     return $Self;
 }
 
-=item Run()
+=head2 Run()
 
 This method handles the event.
 
@@ -90,63 +85,64 @@ sub Run {
 
     # create needed objects
     $Self->{ConfigItemObject} = $Kernel::OM->Get('Kernel::System::ITSMConfigItem');
-    $Self->{LinkObject} = $Kernel::OM->Get('Kernel::System::LinkObject');
-    $Self->{ConfigObject} = $Kernel::OM->Get('Kernel::Config');
+    $Self->{LinkObject}       = $Kernel::OM->Get('Kernel::System::LinkObject');
+    $Self->{ConfigObject}     = $Kernel::OM->Get('Kernel::Config');
 
     # Get Config options
     my $LicenseDefaultSettings = $Self->{ConfigObject}->Get('ITSMConfigItem::CILicenseDefaultSettings');
     my $LicenseLinkTypSettings = $Self->{ConfigObject}->Get('ITSMConfigItem::CILicenseLinkTypSettings');
-    my $LinkTypSoure = $LicenseLinkTypSettings->{Source};
-    my $LinkTypTarget = $LicenseLinkTypSettings->{Target};
+    my $LinkTypSoure           = $LicenseLinkTypSettings->{Source};
+    my $LinkTypTarget          = $LicenseLinkTypSettings->{Target};
 
     my %ClassFilter;
     my $LinkedClassRestrictions = $Self->{ConfigObject}->Get('ITSMConfigItem::CILicenseLinkedClassRestrictions');
-    if ( $LinkedClassRestrictions ) {
-        %ClassFilter = map { $_ => 1 } @{ $LinkedClassRestrictions };
+    if ($LinkedClassRestrictions) {
+        %ClassFilter = map { $_ => 1 } @{$LinkedClassRestrictions};
     }
 
     my $LicenseDeplTypSettings = $Self->{ConfigObject}->Get('ITSMConfigItem::CILicenseDeploymentTypSettings');
-    my $Counter = 0;
+    my $Counter                = 0;
 
     my $ConfigItem;
-    if ($Param{Data}{ConfigItemID}) {
+    if ( $Param{Data}{ConfigItemID} ) {
 
         $ConfigItem = $Self->{ConfigItemObject}->ConfigItemGet(
             ConfigItemID => $Param{Data}{ConfigItemID},
         );
-    
-       if ($ConfigItem->{Class} ne $LicenseDefaultSettings->{CounterClassName}) {
-           return;
-       }
+
+        if ( $ConfigItem->{Class} ne $LicenseDefaultSettings->{CounterClassName} ) {
+            return;
+        }
     }
 
     my $LinkList = $Self->{LinkObject}->LinkListWithData(
-        Object    => 'ITSMConfigItem',
-        Key       => $Param{Data}{ConfigItemID},
-        Object2   => 'ITSMConfigItem',         # (optional)
-        State     => 'Valid',
-        UserID    => 1,
+        Object  => 'ITSMConfigItem',
+        Key     => $Param{Data}{ConfigItemID},
+        Object2 => 'ITSMConfigItem',             # (optional)
+        State   => 'Valid',
+        UserID  => 1,
     );
 
-    if (!$LinkList->{ITSMConfigItem}->{$LinkTypSoure} ) {
-        $ConfigItem->{$LicenseDefaultSettings->{CounterFieldNameUsedLicense}} = 0;
-    } else {
+    if ( !$LinkList->{ITSMConfigItem}->{$LinkTypSoure} ) {
+        $ConfigItem->{ $LicenseDefaultSettings->{CounterFieldNameUsedLicense} } = 0;
+    }
+    else {
 
         LINKTYPE:
-        for my $LinkType ( keys %{$LinkList->{ITSMConfigItem}->{$LinkTypSoure}} ) {
+        for my $LinkType ( keys %{ $LinkList->{ITSMConfigItem}->{$LinkTypSoure} } ) {
 
             # Check if the license is inside the config active
             LINKTYPECONFIG:
             for my $LinkTypeConfig ( keys %{$LicenseLinkTypSettings} ) {
                 if ( $LinkType eq $LinkTypeConfig ) {
                     CI:
-                    for my $LinkTypeDirection (keys %{$LinkList->{ITSMConfigItem}->{$LinkTypSoure}->{$LinkType}} ) {
+                    for my $LinkTypeDirection ( keys %{ $LinkList->{ITSMConfigItem}->{$LinkTypSoure}->{$LinkType} } ) {
                         next CI if ( %ClassFilter && !$ClassFilter{ $LinkList->{ITSMConfigItem}->{$LinkTypSoure}->{$LinkType}->{$LinkTypeDirection}->{Class} } );
 
-                        for my $ConfigDeplState (keys %{$LicenseDeplTypSettings}) {
-                            if ($LinkList->{ITSMConfigItem}->{$LinkTypSoure}->{$LinkType}->{$LinkTypeDirection}->{CurDeplState} eq $ConfigDeplState ) {
+                        for my $ConfigDeplState ( keys %{$LicenseDeplTypSettings} ) {
+                            if ( $LinkList->{ITSMConfigItem}->{$LinkTypSoure}->{$LinkType}->{$LinkTypeDirection}->{CurDeplState} eq $ConfigDeplState ) {
                                 $Counter++;
-                            } 
+                            }
                         }
                     }
                 }
@@ -154,22 +150,22 @@ sub Run {
         }
     }
 
-    $ConfigItem->{$LicenseDefaultSettings->{CounterFieldNameUsedLicense}} = $Counter;
-    my $Stock = $ConfigItem->{$LicenseDefaultSettings->{CounterFieldNameStockLicense}} || 0;
-    if ( $Stock > 0) {
-       $ConfigItem->{$LicenseDefaultSettings->{CounterFieldNameAvailibleLicense}} = $Stock - $Counter;
+    $ConfigItem->{ $LicenseDefaultSettings->{CounterFieldNameUsedLicense} } = $Counter;
+    my $Stock = $ConfigItem->{ $LicenseDefaultSettings->{CounterFieldNameStockLicense} } || 0;
+    if ( $Stock > 0 ) {
+        $ConfigItem->{ $LicenseDefaultSettings->{CounterFieldNameAvailibleLicense} } = $Stock - $Counter;
     }
 
     # add a new version with the new incident state
     my $VersionID = $Self->{ConfigItemObject}->ConfigItemUpdate(
         %{$ConfigItem},
-        UserID      => 1,
+        UserID => 1,
     );
 
     return 1;
 }
 
-=item _ConfigItemDelete()
+=head2 _ConfigItemDelete()
 
 history's event handler for ConfigItemDelete
 
@@ -186,7 +182,7 @@ sub _ConfigItemDelete {
     return 1;
 }
 
-=item _HistoryAdd()
+=head2 _HistoryAdd()
 
 history's default event handler.
 
